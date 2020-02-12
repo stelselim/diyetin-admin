83161d02b65fc0e3cb4a5603f009d84db9bf94f0
Berat Kaya <beratkaya1998@gmail.com>
2020-02-12 20:48:53 +0300

    Added react router and removed unnecessary files

---
+++ src/AppNav.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+export class AppNav extends Component {
+    render() {
+        return (
+            <Router>
+                <Route/>
+            </Router>
+        )
+    }
+}
+
+export default AppNav
