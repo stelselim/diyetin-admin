8859b60ef4cb954255f2739e3871d8bf92f9f5b1
Selim <selim.ustel1998@gmail.com>
2020-02-14 22:01:34 +0300

    Blog Deletion and update functions added

--- src/backend/FirebaseBlog.ts
+++ src/backend/FirebaseBlog.ts
@@ -1,14 +1,13 @@
-import firebase from 'firebase';
-import {async} from 'q';
+import firebase, { firestore } from 'firebase';
 
-class FirebaseBlogOperations{
+class FirebaseBlogOperations {
 
-    
+
     authFirebase: firebase.auth.Auth;
     firestoreFirebase: firebase.firestore.Firestore;
     storageFirebase: firebase.storage.Storage;
 
-    constructor(){
+    constructor() {
         this.firestoreFirebase = firebase.firestore();
         this.authFirebase = firebase.auth();
         this.storageFirebase = firebase.storage();
@@ -24,32 +23,58 @@ class FirebaseBlogOperations{
      * @param {string} references the sources for blog.
      * @param {string} author  name & surname of blog writer
      */
-    addNewBlog = async(UserCredential: firebase.auth.UserCredential, header: string, references: string, imageUrl: string, mainIdea: string, blogText: string,author: string) =>{ 
-        if(header.length>100){
-            throw("Header should be less than 100 Characters")
+    addNewBlog = async (UserCredential: firebase.auth.UserCredential, header: string, references: string, imageUrl: string, mainIdea: string, blogText: string, author: string) => {
+        if (header.length > 100) {
+            throw ("Header should be less than 100 Characters")
         }
 
-    await this.firestoreFirebase.collection("/BeslenmeApp/AllDatas/Blog").add({
-        EklenmeTarihi: new Date(),
-        BlogYazısı:blogText,
-        AnaDüşünce:mainIdea,
-        Başlık:header,
-        Kaynaklar:references,
-        Resim:imageUrl,
-        Yazar: author
-    })
-       
+        await this.firestoreFirebase.collection("/BeslenmeApp/AllDatas/Blog").add({
+            EklenmeTarihi: new Date(),
+            BlogYazısı: blogText,
+            AnaDüşünce: mainIdea,
+            Başlık: header,
+            Kaynaklar: references,
+            Resim: imageUrl,
+            Yazar: author
+        })
+
     }
 
 
     /**
-     * 
+     * @param {firestore.DocumentSnapShot} documentReference this is the reference of the blog post that would like to update
+     * @param {string} header  Max: 100 Characters - this is header of blog.
+     * @param {string} mainIdea mainIdea summarize what the blog is about.
+     * @param {string} blogText the text of blog.
+     * @param {string} references the sources for blog.
+     * @param {string} author  name & surname of blog writer
+     * @param {string} imageUrl this is image url which is from image in Firebase Storage.
      */
-    updateBlogPost = async()=>{}
-    deleteBlogPost = async()=>{}
+    updateBlogPost = async (imageUrl: string,header: string,mainIdea: string, blogText: string, author: string, references: string, documentReference: firestore.DocumentSnapshot) => {
 
-}
+        await this.firestoreFirebase.doc(documentReference.ref.path).update(
+            {
+                BlogYazısı: blogText,
+                AnaDüşünce: mainIdea,
+                Başlık: header,
+                Resim: imageUrl,
+                Kaynaklar: references,
+                Yazar: author
 
-class FirebaseBlogStatistics{
-    
+            }
+        )
+
+
+    }
+
+
+    /**
+     * @param {firestore.DocumentSnapshot} documentReference this is the document reference of the blog post that would like to delete.
+     */
+    deleteBlogPost = async (documentReference: firestore.DocumentSnapshot) => {
+
+        let documentPath = documentReference.ref.path;
+        await this.firestoreFirebase.doc(documentPath).delete();
+    }
+
 }
---
+++ src/backend/FirebaseFAQ.ts
@@ -0,0 +1,33 @@
+import firebase from 'firebase';
+
+
+class FirebaseFAQ{
+    
+
+    /**
+     * Doğru Bilinen Yanlışlar
+     * @param 
+     */
+    falseKnownTrue = () =>{
+
+    }
+
+
+    gainWeight = () =>{
+
+    }
+
+    looseWeight = () =>{
+
+    }
+
+    healthLife = () =>{
+
+    }
+
+    interestingFacts =  () =>{
+
+    }
+
+}
+
