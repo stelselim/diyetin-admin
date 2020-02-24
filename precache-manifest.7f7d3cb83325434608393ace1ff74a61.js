3e7513f1aaa82468515a52131d37e484731bbeb2
Berat Kaya <beratkaya1998@gmail.com>
2020-02-24 20:46:53 +0300

    Updates

---
+++ precache-manifest.7f7d3cb83325434608393ace1ff74a61.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62a494dcb570f092177f4913cfbba92b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "17147509faa7eb2264f0",
+    "url": "./static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "c387bda6c0dc03d752ae",
+    "url": "./static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "17147509faa7eb2264f0",
+    "url": "./static/js/2.fdccd46e.chunk.js"
+  },
+  {
+    "revision": "1c7f0ad687308896ffe8c0ba36060daa",
+    "url": "./static/js/2.fdccd46e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c387bda6c0dc03d752ae",
+    "url": "./static/js/main.edc6c237.chunk.js"
+  },
+  {
+    "revision": "04c56a4d583b5feb2e36",
+    "url": "./static/js/runtime-main.f51bd641.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.fb6bf031c282797767e6fd3852fab2f9.js"
+  "./precache-manifest.7f7d3cb83325434608393ace1ff74a61.js"
 );
 
 self.addEventListener('message', (event) => {
