5f85ffae5296d0041b1f6db9f44a034e2b22a917
stelselim <stelselim@Stelselims-MacBook-Pro.local>
2020-06-07 20:39:24 +0300

    Updates

--- precache-manifest.3d86a96873e4dab1e5ae16419d3e12f2.js
+++ precache-manifest.3d86a96873e4dab1e5ae16419d3e12f2.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "916ae45bb319cb9e9523aa63475b1a68",
+    "revision": "1f242c0834588ea6e9655f48b187b3a3",
     "url": "./index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/css/2.47e06e2e.chunk.css"
   },
   {
-    "revision": "f46a6b41f808177451db",
+    "revision": "3ba95b0f3565e8023596",
     "url": "./static/css/main.5ecd60fb.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/js/2.882e4f37.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "f46a6b41f808177451db",
-    "url": "./static/js/main.6605beaa.chunk.js"
+    "revision": "3ba95b0f3565e8023596",
+    "url": "./static/js/main.e7450198.chunk.js"
   },
   {
     "revision": "04c56a4d583b5feb2e36",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.fa4764b165c388b5ed7a6fcb6ed3550a.js"
+  "./precache-manifest.3d86a96873e4dab1e5ae16419d3e12f2.js"
 );
 
 self.addEventListener('message', (event) => {
